50b47dbf54fe679bc8cdf54f73602aaa7ee49f5c
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-08-07 17:35:31 +0530

    first commit UC 1

---
+++ ConstantAdress.java
@@ -0,0 +1,19 @@
+class AdressBook {
+	long phoneNumber;
+	String city;
+	int pin;
+	String email;
+	String state;
+	String firstName;
+	String lastName;
+	String address;
+}
+
+public class ConstantAdress {
+
+	public static void main(String[] args) {
+		System.out.println(" Welcome to Address Book Program ");
+
+	}
+
+}
